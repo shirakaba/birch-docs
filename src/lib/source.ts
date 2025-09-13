f5186413cf73daf3ab5d74d4e86e47c7a3d48aab
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-09-13 18:33:07 +0900

    `knownIcon` convenience prop

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,9 +1,22 @@
-import { docs } from '@/.source';
-import { loader } from 'fumadocs-core/source';
+import { docs } from "@/.source";
+import { loader } from "fumadocs-core/source";
+import { icons } from "lucide-react";
+import { createElement } from "react";
 
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
   // it assigns a URL to your pages
-  baseUrl: '/docs',
+  baseUrl: "/docs",
   source: docs.toFumadocsSource(),
+
+  // This is for setting icons in the sidebar, based off the `icon` frontmatter
+  // in pages and meta files.
+  // https://fumadocs.dev/docs/headless/source-api#icons
+  icon(icon) {
+    if (!icon || !(icon in icons)) {
+      return null;
+    }
+
+    return createElement(icons[icon as keyof typeof icons]);
+  },
 });
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,10 +1,32 @@
-import defaultMdxComponents from 'fumadocs-ui/mdx';
-import type { MDXComponents } from 'mdx/types';
+import type { CardProps } from "fumadocs-ui/components/card";
+import defaultMdxComponents from "fumadocs-ui/mdx";
+import { icons } from "lucide-react";
+import type { MDXComponents } from "mdx/types";
+import { createElement, type ReactNode } from "react";
+
+const Card = defaultMdxComponents.Card;
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    Card: function BirchCard({
+      children,
+      icon,
+      knownIcon,
+      ...rest
+    }: CardProps & { knownIcon?: keyof typeof icons }) {
+      const resolvedIcon: ReactNode =
+        knownIcon && knownIcon in icons
+          ? createElement(icons[knownIcon])
+          : icon;
+
+      return (
+        <Card {...rest} icon={resolvedIcon}>
+          {children}
+        </Card>
+      );
+    },
     ...components,
   };
 }
