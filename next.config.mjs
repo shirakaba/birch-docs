62a64b88b7a3b11e6eda70b2e3f7a28e21806d21
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-10-02 00:08:44 +0900

    try a few options on Birchlabs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,13 @@ const config = {
   // https://fumadocs.dev/docs/ui/static-export
   output: "export",
   reactStrictMode: true,
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  trailingSlash: true,
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  skipTrailingSlashRedirect: true,
+
+  // Force Next.js to use relative paths for static assets
+  assetPrefix: "https://birchlabs.co.uk/jamie/docs",
 };
 
 export default withMDX(config);
