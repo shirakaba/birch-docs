57bbe2e661d7aaa07a32ff80386881c62ddf006c
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-10-02 00:58:17 +0900

    add logo

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,4 +1,19 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import Image from "next/image";
+
+import Logo from "@/public/logo.png";
+
+// https://github.com/fuma-nama/fumadocs/blob/6c3bde5b4d95c9fd854815be9d94968ac6a39c18/apps/docs/lib/layout.shared.tsx#L39C1-L51C3
+// https://www.flaticon.com/free-icon/birch-tree_1846071?related_id=1846074&origin=search
+export const logo = (
+  <Image
+    width="20"
+    height="20"
+    src={Logo}
+    alt="Logo"
+    className="rounded-full bg-cyan-100 dark:bg-blue-950"
+  />
+);
 
 /**
  * Shared layout configurations
@@ -12,15 +27,8 @@ export function baseOptions(): BaseLayoutProps {
     nav: {
       title: (
         <>
-          <svg
-            width="24"
-            height="24"
-            xmlns="http://www.w3.org/2000/svg"
-            aria-label="Logo"
-          >
-            <circle cx={12} cy={12} r={12} fill="currentColor" />
-          </svg>
-          Birchdocs
+          {logo}
+          <span className="font-medium [header_&]:text-[15px]">Birchdocs</span>
         </>
       ),
     },
