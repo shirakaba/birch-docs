7862fadca05843a583572893f1094b6cb761c0a9
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-10-02 01:19:13 +0900

    try making dark mode-aware favicons

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,6 +1,9 @@
 // Based on https://github.com/fuma-nama/fumadocs/blob/e0cfcdcdd54603bfacd7e016d29b658bdc6c74e4/apps/docs/lib/metadata.ts
 import type { Metadata } from "next/types";
 
+import DarkIcon from "@/public/favicon-dark.png";
+import LightIcon from "@/public/favicon-light.png";
+
 /**
  *
  * @see https://fumadocs.dev
@@ -9,5 +12,23 @@ import type { Metadata } from "next/types";
 export function createMetadata(override: Metadata): Metadata {
   return {
     ...override,
+    icons: {
+      // These aren't working, so consider embedding a PNG inside an SVG
+      // https://evilmartians.com/chronicles/how-to-favicon-in-2021-six-files-that-fit-most-needs
+      icon: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          url: LightIcon.src,
+          media: "(prefers-color-scheme: light)",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          url: DarkIcon.src,
+          media: "(prefers-color-scheme: dark)",
+        },
+      ],
+    },
   };
 }
