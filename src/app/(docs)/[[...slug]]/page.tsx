56a5dd53c53d8e5b00900bf8cf46b32f14853a68
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-10-04 10:29:23 +0900

    add <DocsCategory /> to index pages

--- src/app/(docs)/[[...slug]]/page.tsx
+++ src/app/(docs)/[[...slug]]/page.tsx
@@ -1,3 +1,5 @@
+import { getPageTreePeers } from "fumadocs-core/server";
+import { Card, Cards } from "fumadocs-ui/components/card";
 import { createRelativeLink } from "fumadocs-ui/mdx";
 import {
   DocsBody,
@@ -27,10 +29,28 @@ export default async function Page(props: PageProps<"/[[...slug]]">) {
           components={getMDXComponents({
             // this allows you to link to other pages with relative file paths
             a: createRelativeLink(source, page),
+
+            // https://github.com/fuma-nama/fumadocs/blob/65237fa2f095fe4b6840bb31490fe802bed89158/apps/docs/app/docs/%5B...slug%5D/page.tsx#L121
+            DocsCategory: ({ url }) => {
+              return <DocsCategory url={url ?? page.url} />;
+            },
           })}
         />
       </DocsBody>
     </DocsPage>
+  );
+}
+
+// https://github.com/fuma-nama/fumadocs/blob/65237fa2f095fe4b6840bb31490fe802bed89158/apps/docs/app/docs/%5B...slug%5D/page.tsx#L135
+function DocsCategory({ url }: { url: string }) {
+  return (
+    <Cards>
+      {getPageTreePeers(source.pageTree, url).map((peer) => (
+        <Card key={peer.url} icon={peer.icon} title={peer.name} href={peer.url}>
+          {peer.description}
+        </Card>
+      ))}
+    </Cards>
   );
 }
 
