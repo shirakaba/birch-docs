4fae23a5ca996e5635580877525fe9bdc56403c7
Jamie Birch <14055146+shirakaba@users.noreply.github.com>
2025-10-05 00:29:19 +0900

    improve landing page

--- src/app/(docs)/[[...slug]]/page.tsx
+++ src/app/(docs)/[[...slug]]/page.tsx
@@ -1,4 +1,8 @@
-import { getPageTreePeers } from "fumadocs-core/server";
+import {
+  flattenTree,
+  getPageTreePeers,
+  getPageTreeRoots,
+} from "fumadocs-core/server";
 import { Card, Cards } from "fumadocs-ui/components/card";
 import { createRelativeLink } from "fumadocs-ui/mdx";
 import {
@@ -31,8 +35,8 @@ export default async function Page(props: PageProps<"/[[...slug]]">) {
             a: createRelativeLink(source, page),
 
             // https://github.com/fuma-nama/fumadocs/blob/65237fa2f095fe4b6840bb31490fe802bed89158/apps/docs/app/docs/%5B...slug%5D/page.tsx#L121
-            DocsCategory: ({ url }) => {
-              return <DocsCategory url={url ?? page.url} />;
+            DocsCategory: ({ url, peerType = "page" }) => {
+              return <DocsCategory url={url ?? page.url} peerType={peerType} />;
             },
           })}
         />
@@ -42,14 +46,57 @@ export default async function Page(props: PageProps<"/[[...slug]]">) {
 }
 
 // https://github.com/fuma-nama/fumadocs/blob/65237fa2f095fe4b6840bb31490fe802bed89158/apps/docs/app/docs/%5B...slug%5D/page.tsx#L135
-function DocsCategory({ url }: { url: string }) {
+function DocsCategory({
+  url,
+  peerType,
+}: {
+  url: string;
+  peerType: "folder" | "page" | "any";
+}) {
+  const peers =
+    peerType === "page"
+      ? getPageTreePeers(source.pageTree, url)
+      : peerType === "folder"
+        ? getPageTreeRoots(source.pageTree)
+        : source.pageTree.children;
+
   return (
     <Cards>
-      {getPageTreePeers(source.pageTree, url).map((peer) => (
-        <Card key={peer.url} icon={peer.icon} title={peer.name} href={peer.url}>
-          {peer.description}
-        </Card>
-      ))}
+      {peers.map((peer) => {
+        if (!("type" in peer)) {
+          return null;
+        }
+
+        switch (peer.type) {
+          case "folder": {
+            return (
+              <Card
+                key={peer.$id}
+                icon={peer.icon}
+                title={peer.name}
+                href={flattenTree([peer]).at(0)?.url}
+              >
+                {peer.description}
+              </Card>
+            );
+          }
+          case "page": {
+            return (
+              <Card
+                key={peer.url}
+                icon={peer.icon}
+                title={peer.name}
+                href={peer.url}
+              >
+                {peer.description}
+              </Card>
+            );
+          }
+          case "separator": {
+            return null;
+          }
+        }
+      })}
     </Cards>
   );
 }
